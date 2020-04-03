a5afa01d8081bd5cc66dbb8e2e8dcee9276e96fc
Lauren <lauren.finley1@yahoo.com>
2020-04-03 17:01:00 -0500

    finish styling and connect to JSONPlaceholder REST API

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 const container = document.getElementById("container");
+const numberOfPostsToFetch = 4;
+let pageNumber = 1;
+
+async function getPosts() {
+     let res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${numberOfPostsToFetch}&_page=${pageNumber}`);
+     let data = await res.json();
+     console.log(res);
+     console.log(data);
+     return data;
+}
+
+async function showPosts() {
+    let posts = await getPosts();
+
+    posts.forEach(post => {
+        const element = document.createElement("div");
+        element.classList.add("post");
+        element.innerHTML = `<h2>${post.title}</h2><p>${post.body}</p>`
+    
+        container.appendChild(element);
+    });
+    pageNumber++;
+}
+
+showPosts();
